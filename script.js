bf5f9faf10e8e3dd179d54b8905057688436246e
Ezhilananthsakthivel <71553660+Ezhilananthsakthivel@users.noreply.github.com>
2022-12-14 21:18:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+// //first method it's have some errors------>
+// var ans = ''
+// var exp = /[^+-/%*]/g
+
+// function number(n) {
+//     if (document.getElementById('display').value == 0)
+//         document.getElementById('display').value = ''
+//     ans = document.getElementById('display').value += n
+// }
+
+// function eql(val) {
+//     let input = ''
+//     let arr = []
+//     let answer = 0
+//     val = val.split("").map((v) => {
+//         if (v.match(exp)) {
+//             input += v
+//         }
+//         else {
+//             arr.push(input)
+//             arr.push(v)
+//             input = ''
+//         }
+//     })
+//     // for (let i = 0; i < val.length; i++) {
+//     //     if (val[i].match(exp)) {
+//     //         input += val
+//     //     }
+//     //     else if (val[i] == '.') {
+//     //         val[i - 1] += val[i]
+//     //         val[i - 1] += val[i + 1]
+//     //         i++
+//     //     }
+//     //     else {
+//     //         arr.push(input)
+//     //         arr.push(val[i])
+//     //         input = ''
+//     //     }
+//     // }
+
+//     if (input != '') {
+//         arr.push(input)
+//         input = ''
+//     }
+
+//     answer = parseFloat(arr[0]);
+//     for (var i = 2; i < arr.length; i = i + 2) {
+
+//         switch (arr[i - 1]) {
+//             case '+':
+//                 answer += parseFloat(arr[i]);
+//                 break;
+//             case '-':
+//                 answer -= parseFloat(arr[i]);
+//                 break;
+//             case '/':
+//                 answer = answer / parseFloat(arr[i]);
+//                 break;
+//             case '*': answer = answer * parseFloat(arr[i]);
+//                 break;
+//             case '%': answer = answer % parseFloat(arr[i]);
+//                 break;
+//         }
+
+//     }
+//     document.getElementById('display').value = String(answer)
+// }
+
+// function clr() {
+//     arr = [];
+//     ans = '';
+//     document.getElementById('display').value = ""
+// }
+
+// function del() {
+//     let dis = document.getElementById('display')
+//     dis.value = dis.value.slice(0, -1)
+//     ans = dis.value
+// }
+//---------------------------------------------------------------------------------------->
+
+//second Method--------------------->
+let dis = document.getElementById('display')
+let ans = ''
+function number(n) {
+    if (dis.value == 0)
+        dis.value = ''
+    ans = dis.value += n
+}
+
+function eql(arr) {
+    dis.value = eval(arr)
+}
+
+function clr() {
+    ans = ''
+    dis.value = ''
+}
+function del() {
+    dis.value = dis.value.slice(0, -1)
+    ans = dis.value
+}
